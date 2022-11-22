afc8c9cd5ade8c4466d2361814698d3e896a2ed8
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-11-22 16:54:22 +0100

    corretto l'errore del immagine

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ createApp({
     data (){ 
     return{
         messaggio: "Ciao! Scegli una destinazione!",
-        fotoVietnam: "img/vientam.jpg",
+        fotoVietnam: "https://i.picsum.photos/id/46/3264/2448.jpg?hmac=ZHE8nk-Q9uRp4MxgKNvN7V7pYFvA-9BCv99ltY3HBv4",
         fotoBali: "img/bali.jpg",
         fotoNepal: "img/nepal.jpg",
         fotoGiappone: "img/giappone.jpg",
